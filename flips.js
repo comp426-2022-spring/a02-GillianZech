ecffb14a2e552ab1610f45e56e5e8d2cdf8bee3f
Gillian Zech <ginger01@email.unc.edu>
2022-02-08 15:04:29 -0500

    flips.js finally works importing minimist

--- flips.js
+++ flips.js
@@ -1,7 +1,17 @@
 import { coinFlip } from "./modules/coin.mjs";
 import { coinFlips } from "./modules/coin.mjs";
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 
 // Take one argument on the command line named --number.
 // If no argument is given, it should flip once by default.
 // The result on STDOUT should be an array of all of the flips in sequence followed by an object summarizing the flips.
 
+const args = require('minimist')(process.argv.slice(2));
+args['number'];
+const num = args.number;
+if (num == null) {
+    console.log(coinFlips(1));
+} else {
+    console.log(coinFlips(num));
+}
