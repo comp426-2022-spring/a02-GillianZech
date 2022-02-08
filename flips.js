33f6def5691da8440282c95911e8afccfe2771b6
Gillian Zech <ginger01@email.unc.edu>
2022-02-07 22:45:40 -0500

    Fixed import typos

--- flips.js
+++ flips.js
@@ -1,5 +1,5 @@
-import { coinFlip } from ".modules/coin.mjs";
-import { coinFlips } from ".modules/coin.mjs";
+import { coinFlip } from "./modules/coin.mjs";
+import { coinFlips } from "./modules/coin.mjs";
 
 // Take one argument on the command line named --number.
 // If no argument is given, it should flip once by default.
