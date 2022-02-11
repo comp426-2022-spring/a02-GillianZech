43aa94929b8653db7a0416e8b101fd06c56fae19
Gillian Zech <ginger01@email.unc.edu>
2022-02-11 13:31:32 -0500

    rerun autograder

--- guess-flip.js
+++ guess-flip.js
@@ -3,7 +3,7 @@ import { flipACoin } from "./modules/coin.mjs";
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
-// Take one argument on the command line named --call that will only accept .
+// Take one argument on the command line named --call that will only accept heads or tails.
 // If no argument is given, or something other than “heads” or “tails” is passed with --call, return an error message (see below for example).
 // Compare the call with a flip and report the result to STDOUT (see below)
 
